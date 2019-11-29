99fcfb0a1776c621ac854cb1e640a925fe5b417c
Олег <57451034+Oleg-che@users.noreply.github.com>
2019-11-29 17:26:33 +0300

    Create tiket.java

---
+++ tiket.java
@@ -0,0 +1,14 @@
+package com.cherednik;
+
+public class Average {
+
+    public static void main(String[] args) {
+        int luckyTicket = 0;
+        for (int ticketNumber = 1; ticketNumber < 1000000; ticketNumber = ticketNumber + 1) {
+            if (ticketNumber / 100000 + (ticketNumber / 10000) % 10 + (ticketNumber / 1000) % 10 == (ticketNumber / 100) % 10 + (ticketNumber / 10) % 10 + ticketNumber % 10) {
+                luckyTicket = luckyTicket + 1;
+            }
+        }
+        System.out.println("Количество счастливых билетов в серии :" + luckyTicket);
+    }
+}
